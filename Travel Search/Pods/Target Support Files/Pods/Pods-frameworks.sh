cc92318f793e3656caa8a2192272c99d7637392e
Yahia Mahrous <yahia@mrxprt.com>
2016-06-14 14:35:13 +0300

    [ Add ] ABCustomUINavigationController Library

--- Travel Search/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Travel Search/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/ABCustomUINavigationController.framework"
   install_framework "Pods/Alamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/ABCustomUINavigationController.framework"
   install_framework "Pods/Alamofire.framework"
 fi
